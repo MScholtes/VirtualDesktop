be087c78986a6faeae2b0cc7f299faf39055465f
MScholtes <markusscholtes@hotmail.com>
2018-10-09 17:15:40 +0200

    Update Compile.bat
    COM Interface changed with Windows 10 1809

--- Compile.bat
+++ Compile.bat
@@ -1,4 +1,5 @@
 @echo off
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop1709.cs"
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop.cs"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop1800.cs"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop1709.cs"
 pause
