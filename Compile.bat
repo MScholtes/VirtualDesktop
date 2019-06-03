9ac2505d06e2e8e83d02e6d10eddfb62c20fcf37
MScholtes <markusscholtes@hotmail.com>
2019-06-03 10:52:12 +0200

    Update to V1.4

--- Compile.bat
+++ Compile.bat
@@ -3,5 +3,5 @@
 
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop.cs"
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop1803.cs"
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop1709.cs"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop1607.cs"
 pause
