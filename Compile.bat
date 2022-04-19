e483aaee9f1a3e1b57ca3bb04dc72f47708cb763
MScholtes <markusscholtes@hotmail.com>
2022-04-19 09:03:29 +0200

    Update to V1.9c

--- Compile.bat
+++ Compile.bat
@@ -1,11 +1,12 @@
 @echo off
-:: Markus Scholtes, 2021
+:: Markus Scholtes, 2022
 :: Compile VirtualDesktop in .Net 4.x environment
 setlocal
 
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop11Insider.cs" /win32icon:"%~dp0MScholtes.ico"
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop11.cs" /win32icon:"%~dp0MScholtes.ico"
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop.cs" /win32icon:"%~dp0MScholtes.ico"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktopServer2022.cs" /win32icon:"%~dp0MScholtes.ico"
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop1803.cs" /win32icon:"%~dp0MScholtes.ico"
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop1607.cs" /win32icon:"%~dp0MScholtes.ico"
 
