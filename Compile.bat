342b1328b3a4c6dba332d391e7c69a8d0ddf9c15
DuncanL <duncan.lilly@gmail.com>
2025-01-18 13:41:50 +0000

    Change icon
    More app specific than the author's initials

--- Compile.bat
+++ Compile.bat
@@ -3,11 +3,11 @@
 :: Compile VirtualDesktop in .Net 4.x environment
 setlocal
 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop11.cs" /win32icon:"%~dp0MScholtes.ico"
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop11-24H2.cs" /win32icon:"%~dp0MScholtes.ico"
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop.cs" /win32icon:"%~dp0MScholtes.ico"
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktopServer2022.cs" /win32icon:"%~dp0MScholtes.ico"
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktopServer2016.cs" /win32icon:"%~dp0MScholtes.ico"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop11.cs" /win32icon:"%~dp0VirtualDesktop.ico"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop11-24H2.cs" /win32icon:"%~dp0VirtualDesktop.ico"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktop.cs" /win32icon:"%~dp0VirtualDesktop.ico"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktopServer2022.cs" /win32icon:"%~dp0VirtualDesktop.ico"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe "%~dp0VirtualDesktopServer2016.cs" /win32icon:"%~dp0VirtualDesktop.ico"
 
 :: was batch started in Windows Explorer? Yes, then pause
 echo "%CMDCMDLINE%" | find /i "/c" > nul
